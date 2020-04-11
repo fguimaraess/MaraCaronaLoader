80948d6f35c88e1669d6f7fe138bae3a92864fce
Luiz Faitanin <lfaitanin@outlook.com>
2020-04-10 23:23:42 -0300

    Mexendo na carga

---
+++ MaraCaronaLoader/Model/TempLoader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaraCaronaLoader.Model
+{
+    public class TempLoader
+    {
+        public int Id { get; set; }
+        public string Json { get; set; }
+    }
+}
--- MaraCaronaLoader/Program.cs
+++ MaraCaronaLoader/Program.cs
@@ -8,6 +8,9 @@ using Npgsql;
 using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
+using RestSharp;
+using System.Net.Http.Headers;
+using IdentityModel.Client;
 
 namespace CrawlerGE
 {
@@ -15,15 +18,43 @@ namespace CrawlerGE
     {
         private static readonly string _connectionString = @"Server=ec2-35-174-88-65.compute-1.amazonaws.com;Port=5432;" +
                     "User Id=odwvjyxqhzgabw;Password=ef7d9e8b0476a76423666b960558df83f905c75c2ca3e2321b7b52d96f9a63b9;Database=d85ko4e25vi0j6;SSL Mode=Require;Trust Server Certificate=true";
+        static readonly HttpClient client = new HttpClient();
+
         static void Main(string[] args)
         {
             CargaAsync();
         }
 
+
+
         private static async Task CargaAsync()
         {
+
             try
             {
+
+                int page = 1;
+                List<string> resposta = new List<string>();
+
+                while (page <= 13)
+                {
+                    string baseUrl = "https://live-score-api.com/api-client/fixtures/matches.json?&key=jxKfM3GpOje11Jbl&secret=gW6tfW4qP72wTiycQirDm72argcsZnOg&competition_id=24&page=" + page;
+                    var client = new RestClient(baseUrl);
+                    var request = new RestRequest(Method.GET);
+
+                    IRestResponse response = client.Execute(request);
+                    resposta.Add(response.Content);
+                    page++;
+                }
+                resposta.ForEach(model =>
+                {
+                    using (NpgsqlConnection conn = new NpgsqlConnection(_connectionString))
+                    {
+                        conn.Open();
+                        var cmd = new NpgsqlCommand($"INSERT INTO CLUBE (Name) VALUES ({clube})");
+                    }
+
+                })
                 var partida = new PartidaDto();
 
                 using (NpgsqlConnection conn = new NpgsqlConnection(_connectionString))
@@ -31,9 +62,9 @@ namespace CrawlerGE
                     conn.Open();
                     var cmd = new NpgsqlCommand(@"CREATE TABLE PARTIDA (
                                                     Id INT GENERATED ALWAYS AS IDENTITY,
-                                                    Team_Home_Id int NOT NULL,
-                                                    Team_Away_Id int NOT NULL,
-                                                    Location VARCHAR,
+                                                    username VARCHAR NOT NULL,
+                                                    password VARCHAR NOT NULL,
+                                                    email VARCHAR,
                                                     Competition VARCHAR,
                                                     Date VARCHAR NOT NULL
                                                 );
@@ -49,9 +80,9 @@ namespace CrawlerGE
             }
             catch (Exception e)
             {
-                throw e; 
+                throw e;
             }
-            
+
         }
 
         private static void MontaEstrutura(PartidaDto partida, NpgsqlCommand conn)
